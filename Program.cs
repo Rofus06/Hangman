9c7cd8143bd028190ab6b2d9f6c9b18cddad983d
Rofus06 <Filipsmail@yahoo.com>
2024-10-10 11:06:52 +0200

    made it a little bit better

--- Program.cs
+++ Program.cs
@@ -33,19 +33,19 @@ class Program
 
             // guess
             Console.Write("Enter a guess: ");
-            string guess = Console.ReadLine();
+            string guess = Console.ReadLine().ToLower(); //bara gjorde så det är littet
 
             // kollar om ordet finns
-            if (word.Contains(guess))
+            if (word.ToLower().Contains(guess))
             {
                 Console.WriteLine("Yes");
 
                 // vissar ordet
                 for (int i = 0; i < word.Length; i++)
                 {
-                    if (word[i].ToString() == guess)
+                    if (word[i].ToString().ToLower() == guess) 
                     {
-                        hiddenWords[i] = guess;
+                        hiddenWords[i] = word[i].ToString();
                     }
                 }
             }
@@ -54,9 +54,15 @@ class Program
                 Console.WriteLine("No");
                 wrongGuesses.Add(guess);
             }
+            // Kollar om user har namnet rätt helt
+            if (string.Join("", hiddenWords) == word)
+            {
+                Console.WriteLine("You won!");
+                break; // Exit the loop för du van
+            }
         }
 
-        Console.WriteLine("Game Over");
+        Console.WriteLine("Game Is Over");
         Console.ReadLine();
     }
 }
